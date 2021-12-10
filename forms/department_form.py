57506abba6c27b6e5ff77e7d5cc59d8365c20e30
rostyk63 <rosmarushko@gmail.com>
2021-12-11 00:25:18 +0200

    refactoring

--- forms/department_form.py
+++ forms/department_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField, DecimalField
-from wtforms.validators import DataRequired, Length, NumberRange, Optional
+from wtforms import StringField, SubmitField, DecimalField
+from wtforms.validators import Length, NumberRange, Optional
 
 
 class DepartmentForm(FlaskForm):
@@ -10,32 +10,17 @@ class DepartmentForm(FlaskForm):
     name = StringField('Name: ',
                        validators=[
                            Length(min=3, max=100,
-                                  message="Name should be from 3 up to 100 symbols"),
-                           Optional()
-                           # DataRequired()
-                       ])
+                                  message="Name should be from 3 up to 100 symbols"), Optional()])
     min_avg_salary = DecimalField('Min avg salary: ',
-                                  validators=[
-                                      NumberRange(min=0, max=100_000, message='Salary should be positive'),
-                                      Optional()
-                                      # DataRequired()
-                                  ])
+                                  validators=[NumberRange(min=0, max=100_000, message='Salary should be positive'),
+                                              Optional()])
     max_avg_salary = DecimalField('Max avg salary: ',
                                   validators=[
-                                      NumberRange(min=0, max=100_000, message='Salary should be positive'),
-                                      Optional()
-                                      # DataRequired()
-                                  ])
+                                      NumberRange(min=0, max=100_000, message='Salary should be positive'), Optional()])
     min_employee = DecimalField('Min amount of employee: ',
                                 validators=[
-                                    NumberRange(min=0, max=100_000, message='Amount of employee should be positive'),
-                                    Optional()
-                                    # DataRequired()
-                                ])
+                                    NumberRange(min=0, max=100_000, message='Amount of employee should be positive'), ])
     max_employee = DecimalField('Max amount of employee: ',
                                 validators=[
-                                    NumberRange(min=0, max=100_000, message='Amount of employee should be positive'),
-                                    Optional()
-                                    # DataRequired()
-                                ])
-    submit = SubmitField('')
+                                    NumberRange(min=0, max=100_000, message='Amount of employee should be positive'), ])
+    find = SubmitField('')
