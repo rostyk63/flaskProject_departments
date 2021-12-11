353fd0dc37519b8d6fb82fe464556c78cdcd5580
rostyk63 <rosmarushko@gmail.com>
2021-12-11 15:53:57 +0200

    populate db

--- models/populate_database.py
+++ models/populate_database.py
@@ -14,22 +14,26 @@ def populate_database():
     department_1 = Department('Epam')
     department_2 = Department('SoftServe')
     department_3 = Department('Sombra')
+    department_4 = Department('GlobalLogic')
 
     employee_1 = Employee('Nenchyn Pavlo', 700, date(2002, 8, 22), department_1)
-    employee_2 = Employee('Oleh Petryliak', 950, date(2002, 10, 4), department_2)
-    employee_3 = Employee('Makam Galant', 700, date(1989, 11, 25), department_3)
+    employee_2 = Employee('Petryliak Oleh', 950, date(2002, 10, 4), department_2)
+    employee_3 = Employee('Galant Makam', 700, date(1989, 11, 25), department_3)
+    employee_4 = Employee('Dmytrasevych Iryna', 5000, date(2002, 6, 5), department_4)
 
     db.session.add(department_1)
     db.session.add(department_2)
     db.session.add(department_3)
+    db.session.add(department_4)
 
     db.session.add(employee_1)
     db.session.add(employee_2)
     db.session.add(employee_3)
+    db.session.add(employee_4)
 
     db.session.commit()
     db.session.close()
 
 
-# if __name__ == '__main__':
-#     populate_database()
+if __name__ == '__main__':
+    populate_database()
