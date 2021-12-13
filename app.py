2f9afac87d45010567e2006204cd483be9870889
rostyk63 <rosmarushko@gmail.com>
2021-12-13 22:26:35 +0200

    extension of functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -36,6 +36,8 @@ def departments():
         if form.max_employee.data or form.max_employee.data == 0:
             name_salary_employees = filter(lambda department: department['employee_count'] <= form.max_employee.data,
                                            name_salary_employees)
+        if form.create_name.data:
+            create_department(form.create_name.data)
     name_salary_employees = list(name_salary_employees)
     return render_template('departments.html', departments=name_salary_employees, form=form)
 
@@ -48,7 +50,6 @@ def employees():
                       'salary': employee.salary, 'date_of_birth': employee.date_of_birth, 'id': employee.id} for
                      employee in
                      employees_list]
-    # print(employee_info)
     if form.validate_on_submit():
         if form.name.data:
             employee_info = filter(lambda employee: employee['name'] == form.name.data, employee_info)
@@ -62,8 +63,6 @@ def employees():
             employee_info = filter(lambda employee: employee['date_of_birth'] >= form.birth_from.data, employee_info)
         if form.birth_to.data:
             employee_info = filter(lambda employee: employee['date_of_birth'] <= form.birth_to.data, employee_info)
-    # form.name.data = ''
-
     employee_info = list(employee_info)
     return render_template('employees.html', employees=employee_info, form=form)
 
@@ -73,27 +72,106 @@ def department_get(department_id):
     department = get_department_by_id(department_id)
     department_info = {'name': department.name, 'avg_salary': int(get_avg_salary(department.id)),
                        'employee_count': get_amount_of_employee(department.id),
-                       'min_salary': get_min_salary(department.id), 'max_salary': get_max_salary(department.id)}
-    # print(department_info)
+                       'min_salary': get_min_salary(department.id), 'max_salary': get_max_salary(department.id),
+                       'id': department.id}
     employees_list = get_employees_by_department(department_id)
     employee_infos = [
         {'name': employee.name, 'salary': employee.salary, 'date_of_birth': employee.date_of_birth, 'id': employee.id}
         for
         employee in
         employees_list]
-    # print(employee_infos)
     return render_template('department.html', department=department_info,
                            employees=employee_infos)
+
+
+@app.route('/departments/<int:department_id>/add', methods=['GET', 'POST'])
+def add_employee(department_id):
+    department = get_department_by_id(department_id)
+    department_info = {'name': department.name, 'avg_salary': int(get_avg_salary(department.id)),
+                       'employee_count': get_amount_of_employee(department.id),
+                       'min_salary': get_min_salary(department.id), 'max_salary': get_max_salary(department.id),
+                       'id': department.id}
+    if request.method == 'POST':
+        employee_name = request.form.get('employee_name')
+        employee_salary = request.form.get('employee_salary')
+        employee_birthday = request.form.get('employee_birthday')
+        add_employee_to_department(employee_name, employee_salary, employee_birthday, department)
+    return render_template('employee_add.html', department=department_info)
 
 
 @app.route('/employees/<int:employee_id>', methods=['GET', 'POST'])
 def employee_get(employee_id):
     employee = get_employee_by_id(employee_id)
-    # print(employee)
     employee_info = {'name': employee.name, 'salary': employee.salary, 'birthday': employee.date_of_birth,
-                     'department': get_employee_department(employee.department_id)[0][0]}
-    # print(employee_info)
+                     'department': get_employee_department(employee.department_id)[0][0], 'id': employee.id}
+    if request.method == 'POST':
+        delete_employee(employee_info['id'])
     return render_template('employee.html', employee=employee_info)
+
+
+@app.route('/employees/<int:employee_id>/edit', methods=['GET', 'POST'])
+def edit_employee(employee_id):
+    employee = get_employee_by_id(employee_id)
+    employee_info = {'name': employee.name, 'salary': employee.salary, 'birthday': employee.date_of_birth,
+                     'department': get_employee_department(employee.department_id)[0][0], 'id': employee.id}
+    return render_template('employee_add.html', employee=employee_info)
+
+
+@app.route('/departments/<int:department_id>/edit', methods=['GET', 'POST'])
+def edit_department(department_id):
+    department = get_department_by_id(department_id)
+    department_info = {'name': department.name, 'avg_salary': int(get_avg_salary(department.id)),
+                       'employee_count': get_amount_of_employee(department.id),
+                       'min_salary': get_min_salary(department.id), 'max_salary': get_max_salary(department.id),
+                       'id': department.id}
+    if request.method == 'POST':
+        department_new_name = request.form.get('department_new_name')
+        rename_department(department_info['id'], department_new_name)
+    return render_template('department_edit.html', department=department_info)
+
+
+@app.route('/employees/<int:employee_id>/edit_name', methods=['GET', 'POST'])
+def edit_employee_name(employee_id):
+    employee = get_employee_by_id(employee_id)
+    employee_info = {'name': employee.name, 'salary': employee.salary, 'birthday': employee.date_of_birth,
+                     'department': get_employee_department(employee.department_id)[0][0], 'id': employee.id}
+    if request.method == 'POST':
+        new_name = request.form.get('employee_new_name')
+        update_employee_name(employee_info['id'], new_name)
+    return render_template('employee_edit_name.html', employee=employee_info)
+
+
+@app.route('/employees/<int:employee_id>/edit_salary', methods=['GET', 'POST'])
+def edit_employee_salary(employee_id):
+    employee = get_employee_by_id(employee_id)
+    employee_info = {'name': employee.name, 'salary': employee.salary, 'birthday': employee.date_of_birth,
+                     'department': get_employee_department(employee.department_id)[0][0], 'id': employee.id}
+    if request.method == 'POST':
+        new_salary = request.form.get('employee_new_salary')
+        update_employee_salary(employee_info['id'], new_salary)
+    return render_template('employee_edit_salary.html', employee=employee_info)
+
+
+@app.route('/employees/<int:employee_id>/edit_birthday', methods=['GET', 'POST'])
+def edit_employee_birthday(employee_id):
+    employee = get_employee_by_id(employee_id)
+    employee_info = {'name': employee.name, 'salary': employee.salary, 'birthday': employee.date_of_birth,
+                     'department': get_employee_department(employee.department_id)[0][0], 'id': employee.id}
+    if request.method == 'POST':
+        new_birthday = request.form.get('employee_new_birthday')
+        update_employee_birthday(employee_info['id'], new_birthday)
+    return render_template('employee_edit_birthday.html', employee=employee_info)
+
+
+@app.route('/employees/<int:employee_id>/edit_department', methods=['GET', 'POST'])
+def edit_employee_department(employee_id):
+    employee = get_employee_by_id(employee_id)
+    employee_info = {'name': employee.name, 'salary': employee.salary, 'birthday': employee.date_of_birth,
+                     'department': get_employee_department(employee.department_id)[0][0], 'id': employee.id}
+    if request.method == 'POST':
+        new_department = request.form.get('employee_new_dep')
+        update_employee_department(employee_info['id'], new_department)
+    return render_template('employee_edit_department.html', employee=employee_info)
 
 
 if __name__ == '__main__':
